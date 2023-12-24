bd832d552d2dd2a656e6905d4d2ba69732c50456
irfan <99653372+MohammedIrfan33@users.noreply.github.com>
2023-12-24 11:41:39 +0530

    initalflutter
    initial commit

---
+++ salon/mycut/android/app/src/main/kotlin/com/example/mycut/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mycut
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ salon/mycut/lib/core/utils/constants/app_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const  Color primaryColor = Color(0xff156779);
+  static const  Color ligtSkyBlue = Color(0xffE2F5FB);
+  static const  Color mediumBlack = Color(0xff292929);
+  static const  Color darkGary = Color(0xff545454);
+  static const  Color lightGray = Color(0xff9B9B9B);
+  static const  Color white10 = Color(0xffF1F5F5);
+  static const Color black = Color(0xff000000);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color success = Color(0xffF21A1A);
+  static const Color error = Color(0xff138F4D);
+
+
+}
